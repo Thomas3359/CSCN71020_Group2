c0c7081346f9a4496532607a63f575cec3f3655a
Thomas Amiro <Tamiro3359@conestogac.on.ca>
2022-11-24 18:01:50 -0800

    Added the test cases for the analyzeTriangle function (does not work). Added 4 test cases for each type of triangle. Also did the properties stuff that is in the video (Week 7 Lab).

---
+++ TRI_001/TRI_001.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" int analyzeTriangle(int side1, int side2, int side3);
+
+// TRI_001: Testing for type of triangle
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TRI001
+{
+	TEST_CLASS(TRI001)
+	{
+	public:
+		
+		TEST_METHOD(TypeOfTriangle)
+		{
+			//This is testing the "Equilateral triangle" portion of the Analyze Triangle Function
+			int Result = 0;
+			Result = analyzeTriangle(0, 0, 0);
+			Assert::IsTrue("Equilateral triangle", Result);
+		}
+
+		TEST_METHOD(TypeOfTriangle2)
+		{
+			//This is testing the "Isosceles triangle" portion of the Analyze Triangle Function
+			int Result = 0;
+			Result = analyzeTriangle(0, 0, 0);
+			Assert::IsTrue("Isosceles triangle", Result);
+		}
+
+		TEST_METHOD(TypeOfTriangle3)
+		{
+			//This is testing the "Scalene triangle" portion of the Analyze Triangle Function
+			int Result = 0;
+			Result = analyzeTriangle(0, 0, 0);
+			Assert::IsTrue("Scalene triangle", Result);
+		}
+		TEST_METHOD(TypeOfTriangle4)
+		{
+			//This is testing the "Not A Triangle" portion of the Analyze Triangle Function
+			int Result = 0;
+			Result = analyzeTriangle(0, 0, 0);
+			Assert::IsTrue("Not a triangle", Result);
+		}
+	};
+}
